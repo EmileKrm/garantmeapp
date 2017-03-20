e6c799e1709c163d5306bbc91de67d7b07902a3a
emile karam <emile.karam@hec.edu>
2017-03-20 14:47:56 +0100

    Designed user and Organisation models and ran migration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
   #Protect your app in views and controllers
   include Pundit
+
+   def configure_permitted_parameters
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :phone_number, :date_of_birth, :home_address, :organisation_name])
+  end
 
   # Pundit: white-list approach.
   after_action :verify_authorized, except: :index, unless: :skip_pundit?
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170319123558) do
+ActiveRecord::Schema.define(version: 20170320132654) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "organisations", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -28,8 +34,16 @@ ActiveRecord::Schema.define(version: 20170319123558) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "phone_number"
+    t.date     "date_of_birth"
+    t.string   "hame_address"
+    t.integer  "organisation_id"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["organisation_id"], name: "index_users_on_organisation_id", using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "users", "organisations"
 end
