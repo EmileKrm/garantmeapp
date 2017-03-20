971dd3868336be610c099b07d2e53f8e770967e7
emile karam <emile.karam@hec.edu>
2017-03-20 15:12:47 +0100

    replaced applications with interviews

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :users only: [:edit, :update] do
-    resources :applications only: [:show, :new, :create, :edit, :update]
+    resources :interviews only: [:show, :new, :create, :edit, :update]
   end
   resources :organisations only: [:index, :show, :new, :create, :edit, :update]
 end
