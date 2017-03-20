c32c6b5b5bb187af9b7f252589d4a9297fb24f3a
Quentin Gondat <quentin.gondat@gmail.com>
2017-03-20 15:31:10 +0100

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20170320132351) do
-
+ActiveRecord::Schema.define(version: 20170320140727) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
 
   create_table "interviews", force: :cascade do |t|
     t.string   "professional_status"
@@ -29,12 +26,12 @@ ActiveRecord::Schema.define(version: 20170320132351) do
     t.datetime "created_at",          null: false
     t.datetime "updated_at",          null: false
     t.index ["user_id"], name: "index_interviews_on_user_id", using: :btree
+  end
 
   create_table "organisations", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-
   end
 
   create_table "users", force: :cascade do |t|
@@ -62,9 +59,6 @@ ActiveRecord::Schema.define(version: 20170320132351) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
-
   add_foreign_key "interviews", "users"
-
   add_foreign_key "users", "organisations"
-
 end
