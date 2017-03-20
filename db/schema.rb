164bf0f1d3640f13b99621f0967d205dd795e783
jonathan parisot <parisot.jonathan@gmail.com>
2017-03-20 14:26:28 +0100

    created the Interview model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170319123558) do
+ActiveRecord::Schema.define(version: 20170320132351) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "interviews", force: :cascade do |t|
+    t.string   "professional_status"
+    t.string   "city"
+    t.date     "move_in_date"
+    t.integer  "monthly_budget"
+    t.integer  "monthly_income"
+    t.boolean  "unique_signator"
+    t.integer  "user_id"
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
+    t.index ["user_id"], name: "index_interviews_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -32,4 +45,5 @@ ActiveRecord::Schema.define(version: 20170319123558) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "interviews", "users"
 end
