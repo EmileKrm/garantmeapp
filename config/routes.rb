fef9f1d7016bb00e4ef97920a6892d6b3328d20f
emile karam <emile.karam@hec.edu>
2017-03-20 15:08:25 +0100

    designed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users only: [:edit, :update] do
+    resources :applications only: [:show, :new, :create, :edit, :update]
+  end
+  resources :organisations only: [:index, :show, :new, :create, :edit, :update]
 end
