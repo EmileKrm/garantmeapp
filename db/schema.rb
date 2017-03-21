babf600f8d46e4d2d4f1a5f79512307035ff34bd
Alexandros Pantalis <alexandros.pantalis@hec.edu>
2017-03-21 16:54:56 +0100

    last debug

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20170321113511) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,11 +40,11 @@ ActiveRecord::Schema.define(version: 20170321113511) do
     t.integer  "user_id"
     t.datetime "created_at",          null: false
     t.datetime "updated_at",          null: false
+    t.integer  "organisation_id"
     t.boolean  "has_found_apartment"
     t.string   "arrondissement"
     t.boolean  "has_a_cosigner"
     t.string   "id_card"
-    t.integer  "organisation_id"
     t.index ["organisation_id"], name: "index_interviews_on_organisation_id", using: :btree
     t.index ["user_id"], name: "index_interviews_on_user_id", using: :btree
   end
@@ -80,8 +78,6 @@ ActiveRecord::Schema.define(version: 20170321113511) do
     t.string   "provider"
     t.string   "uid"
     t.string   "linkedin_picture_url"
-    t.string   "linkedin_first_name"
-    t.string   "linkedin_last_name"
     t.string   "token"
     t.datetime "token_expiry"
     t.string   "photo"
