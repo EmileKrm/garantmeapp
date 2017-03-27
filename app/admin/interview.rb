b79a0d60918de2400fa46831a79e25db0c6039ce
Alexandros Pantalis <alexandros.pantalis@hec.edu>
2017-03-27 17:57:52 +0200

    added new interview fields in edit mode

--- app/admin/interview.rb
+++ app/admin/interview.rb
@@ -26,15 +26,31 @@ ActiveAdmin.register Interview do
     f.inputs "Logistics" do
       f.input :move_in_date
       f.input :has_found_apartment
+      f.input :landlord_email
     end
     f.inputs "Financials" do
       f.input :professional_status
       f.input :monthly_income
       f.input :monthly_budget
+      f.input :monthly_rent
+      f.input :has_a_cosigner
+      f.input :unique_signator
     end
     f.inputs "Geography" do
       f.input :city
       f.input :arrondissement
+      f.input :address
+    end
+    f.inputs "Application info" do
+      f.input :status
+      f.input :user_id
+      f.input :organisation_id
+      f.input :interview_completed
+      f.input :documents_submitted
+      f.input :documents_validated
+      f.input :agreement_signed
+      f.input :lease_provided
+      f.input :lease_signed
     end
     f.actions
   end
