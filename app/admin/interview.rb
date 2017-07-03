02080aa3e880acacfe92e25a16c74b2f396aa88e
emile karam <emile.karam@hec.edu>
2017-07-03 15:17:34 +0200

    fixed display of columns in admin view

--- app/admin/interview.rb
+++ app/admin/interview.rb
@@ -2,7 +2,14 @@ ActiveAdmin.register Interview do
   permit_params :professional_status, :city, :move_in_date, :monthly_budget,
   :monthly_income, :unique_signator, :has_found_apartment, :arrondissement,
   :has_a_cosigner, :address, :status, :landlord_email, :monthly_rent, :interview_completed,
-  :documents_submitted, :documents_validated, :agreement_signed, :lease_provided, :lease_signed
+  :documents_submitted, :documents_validated, :agreement_signed, :lease_provided, :lease_signed,
+  :organisation_id, :interview_id, :organisation, :move_in_date, :created_date,
+  :home_address, :professional_status, :job_title, :work_place, :city, :monthly_budget,
+  :monthly_income, :has_found_apartment, :arrondissement, :address, :appartment_address,
+  :monthly_rent, :landlord_email, :landlord_phone, :provider, :interview_completed,
+  :documents_submitted, :documents_validated, :agreement_signed, :lease_provided, :lease_signed,
+  :has_a_cosigner, :cosigner_first_name, :cosigner_last_name, :cosigner_monthly_income, :cosigner_address,
+  :cosigner_phone, :cosigner_email
 
   # Displayed columns in interview index
   index do
