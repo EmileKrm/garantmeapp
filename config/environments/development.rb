ee5c8fc29dbf583ae9cc555c8dc730c1e4553f7b
emile karam <emile.karam@hec.edu>
2017-07-04 11:54:42 +0200

    implemented mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,6 +34,10 @@ Rails.application.configure do
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
+  #setup letter opener
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
 
